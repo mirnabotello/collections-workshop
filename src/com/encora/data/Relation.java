3e4aabd337b2b726dae68db63761e467bd0a344a
Mirna Botello <mbotello@skytouchtechnology.com>
2022-09-29 17:03:44 -0500

    Get test time

---
+++ src/com/encora/data/Relation.java
@@ -0,0 +1,22 @@
+package com.encora.data;
+
+public class Relation {
+    private String people_id;
+    private String car_vin;
+
+    public String getPeople_id() {
+        return people_id;
+    }
+
+    public void setPeople_id(String people_id) {
+        this.people_id = people_id;
+    }
+
+    public String getCar_vin() {
+        return car_vin;
+    }
+
+    public void setCar_vin(String car_vin) {
+        this.car_vin = car_vin;
+    }
+}
