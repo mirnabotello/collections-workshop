3be8796730096a638e1deb59ff960a17a97b0081
Mirna Botello <mbotello@skytouchtechnology.com>
2022-09-29 17:10:04 -0500

    Load people and relations

--- test/com/encora/CarTest.java
+++ test/com/encora/CarTest.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,18 +17,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.encora.data.Car;
+import com.encora.data.Person;
+import com.encora.data.Relation;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 public class CarTest {
 
     private static List<Car> cars;
+    private static List<Person> people;
+    private static List<Relation> relations;
+
+    private Map<Car, Person> ownersByCar;
+
+    private long creationStartTime;
+    private long creationEndTime;
     private long startTime;
     private long endTime;
 
     @BeforeAll
     static void setup() throws IOException {
 
-        cars = new ArrayList<Car>();
+        cars = new ArrayList<>();
+        people = new ArrayList<>();
 
         Files.list(Paths.get("resources/car/")).forEach(pathToFile -> {
             try {
@@ -37,6 +48,18 @@ public class CarTest {
                 e.printStackTrace();
             }
         });
+
+        Files.list(Paths.get("resources/people/")).forEach(pathToFile -> {
+            try {
+                people.addAll(new CsvToBeanBuilder<Person>(new FileReader(pathToFile.toString()))
+                        .withType(Person.class).build().parse());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+
+        relations = new CsvToBeanBuilder<Relation>(new FileReader("resources/relations.csv"))
+                .withType(Relation.class).build().parse();
     }
 
     @BeforeEach
@@ -47,13 +70,20 @@ public class CarTest {
     @AfterEach
     void endTime() {
         endTime = System.nanoTime();
-        System.out.println("Time to process: " + (endTime - startTime)/1000000);
+        long creationTime = (creationEndTime - creationStartTime)/1000000;
+        long processTime = (endTime - startTime)/1000000;
+
+        System.out.println("Time to create structure: " + creationTime);
+        System.out.println("Complete time: " + processTime);
+        System.out.println("Time using the structure: " + (processTime - creationTime));
     }
 
     @Test
     void validateCarsCount()
     {
         assertEquals(10000, cars.size());
+        assertEquals(10000, people.size());
+        assertEquals(7000, relations.size());
     }
 
 }
